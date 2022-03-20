e5bf814aa4b8fa2487f437c240c9b3ce01ce5460
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2022-03-20 08:23:58 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/Locale/ID.pm
@@ -0,0 +1,52 @@
+package Acme::CPANModules::Locale::ID;
+
+use strict;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+our $LIST = {
+    summary => "List of modules related to the Indonesian locale (language, country)",
+    entries => [
+        {module=>'App::cal::idn'},
+        {module=>'Acme::ID::CompanyName'},
+        {module=>'App::LinguaIDUtils'},
+        {module=>'ArrayData::Lingua::Word::ID::KBBI'},
+        {module=>'ArrayData::Lingua::Word::ID::Wordle'},
+        {module=>'Calendar::Indonesia::Holiday'},
+        {module=>'Graphics::ColorNames::HTML_ID'},
+        {module=>'Graphics::ColorNamesLite::HTML_ID'},
+        {module=>'Lingua::ID::Number::Format::MixWithWords'},
+        {module=>'Lingua::ID::Nums2Words'},
+        {module=>'Lingua::ID::Words2Nums'},
+        {module=>'Locale::ID::District'},
+        {module=>'Locale::ID::GuessGender::FromFirstName'},
+        {module=>'Locale::ID::Locality'},
+        {module=>'Locale::ID::ParseName::Person'},
+        {module=>'Locale::ID::Province'},
+        {module=>'Locale::ID::Village'},
+        {module=>'Parse::Date::Month::ID'},
+        {module=>'Parse::Number::ID'},
+        {module=>'Parse::PhoneNumber::ID'},
+        {module=>'WordList::ID::AnimalName::PERLANCAR'},
+        {module=>'WordList::ID::BIP39'},
+        {module=>'WordList::ID::ColorName::HTML_ID'},
+        {module=>'WordList::ID::ColorName::PERLANCAR'},
+        {module=>'WordList::ID::FruitName::PERLANCAR'},
+        {module=>'WordList::ID::KBBI'},
+        {module=>'WordList::ID::Wordle'},
+        {module=>'WordList::Phrase::ID::Proverb::KBBI'},
+        {module=>'WordLists-ID-Common'},
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
+
+=head1 prepend:SEE ALSO
+
+Other C<Acme::CPANModules::Locale::*>
